dc871ea6498224a165b529e509b29b0da3c4cd59
kitecrimson <kitecrimson@gmail.com>
2016-02-03 10:09:18 +0200

    Artefacts config format

--- cosmos/server/main.js
+++ cosmos/server/main.js
@@ -9,108 +9,524 @@ Game.Cosmos.TIME_COLLECT_ARTEFACTS = 120;
 // If artefact not in list or value <= 0, then artefact never appears at this type of planet
 Game.Cosmos.PLANET_TYPE_ARTEFACTS = {
 	jungle: {
-		white: { weapon_parts: 100, silver_plasmoid: 80, ship_details: 60, meteor_fragments: 40, crystal_fragments: 20 },
-		green: { secret_technology: 100, rotary_amplifier: 80, reptile_technology: 60, emerald_plasmoid: 40, batteries: 20 },
-		blue: { sapphire_plasmoid: 100, quad_cooler: 80, plasma_transistors: 60, nanowires: 40, chip: 20 },
-		purple: { nicolascagium: 100, keanureevesium: 80, jimcarrium: 60, garyoldmanium: 40, amethyst_plasmoid: 20 }, 
-		orange: { topaz_plasmoid: 100, ancient_technology: 80, ancient_scheme: 60, ancient_knowledge: 40, ancient_artefact: 20 },
-		red: { ruby_plasmoid: 100 }
+		white: {
+			weapon_parts: 90, 
+			silver_plasmoid: 80, 
+			ship_details: 60, 
+			meteor_fragments: 40, 
+			crystal_fragments: 20
+		},
+		green: {
+			secret_technology: 90, 
+			rotary_amplifier: 80, 
+			reptile_technology: 60, 
+			emerald_plasmoid: 40, 
+			batteries: 20
+		},
+		blue: {
+			sapphire_plasmoid: 90, 
+			quad_cooler: 80, 
+			plasma_transistors: 60, 
+			nanowires: 40, 
+			chip: 20
+		},
+		purple: {
+			nicolascagium: 90, 
+			keanureevesium: 80, 
+			jimcarrium: 60, 
+			garyoldmanium: 40, 
+			amethyst_plasmoid: 20
+		}, 
+		orange: {
+			topaz_plasmoid: 90, 
+			ancient_technology: 80, 
+			ancient_scheme: 60, 
+			ancient_knowledge: 40, 
+			ancient_artefact: 20 
+		},
+		red: {
+			ruby_plasmoid: 50
+		}
 	},
 	terran: {
-		white: { weapon_parts: 100, silver_plasmoid: 80, ship_details: 60, meteor_fragments: 40, crystal_fragments: 20 },
-		green: { secret_technology: 100, rotary_amplifier: 80, reptile_technology: 60, emerald_plasmoid: 40, batteries: 20 },
-		blue: { sapphire_plasmoid: 100, quad_cooler: 80, plasma_transistors: 60, nanowires: 40, chip: 20 },
-		purple: { nicolascagium: 100, keanureevesium: 80, jimcarrium: 60, garyoldmanium: 40, amethyst_plasmoid: 20 }, 
-		orange: { topaz_plasmoid: 100, ancient_technology: 80, ancient_scheme: 60, ancient_knowledge: 40, ancient_artefact: 20 },
-		red: { ruby_plasmoid: 100 }
+		white: {
+			weapon_parts: 90, 
+			silver_plasmoid: 80, 
+			ship_details: 60, 
+			meteor_fragments: 40, 
+			crystal_fragments: 20
+		},
+		green: {
+			secret_technology: 90, 
+			rotary_amplifier: 80, 
+			reptile_technology: 60, 
+			emerald_plasmoid: 40, 
+			batteries: 20
+		},
+		blue: {
+			sapphire_plasmoid: 90, 
+			quad_cooler: 80, 
+			plasma_transistors: 60, 
+			nanowires: 40, 
+			chip: 20
+		},
+		purple: {
+			nicolascagium: 90, 
+			keanureevesium: 80, 
+			jimcarrium: 60, 
+			garyoldmanium: 40, 
+			amethyst_plasmoid: 20
+		}, 
+		orange: {
+			topaz_plasmoid: 90, 
+			ancient_technology: 80, 
+			ancient_scheme: 60, 
+			ancient_knowledge: 40, 
+			ancient_artefact: 20 
+		},
+		red: {
+			ruby_plasmoid: 50
+		}
 	},
 	ocean: {
-		white: { weapon_parts: 100, silver_plasmoid: 80, ship_details: 60, meteor_fragments: 40, crystal_fragments: 20 },
-		green: { secret_technology: 100, rotary_amplifier: 80, reptile_technology: 60, emerald_plasmoid: 40, batteries: 20 },
-		blue: { sapphire_plasmoid: 100, quad_cooler: 80, plasma_transistors: 60, nanowires: 40, chip: 20 },
-		purple: { nicolascagium: 100, keanureevesium: 80, jimcarrium: 60, garyoldmanium: 40, amethyst_plasmoid: 20 }, 
-		orange: { topaz_plasmoid: 100, ancient_technology: 80, ancient_scheme: 60, ancient_knowledge: 40, ancient_artefact: 20 },
-		red: { ruby_plasmoid: 100 }
+		white: {
+			weapon_parts: 90, 
+			silver_plasmoid: 80, 
+			ship_details: 60, 
+			meteor_fragments: 40, 
+			crystal_fragments: 20
+		},
+		green: {
+			secret_technology: 90, 
+			rotary_amplifier: 80, 
+			reptile_technology: 60, 
+			emerald_plasmoid: 40, 
+			batteries: 20
+		},
+		blue: {
+			sapphire_plasmoid: 90, 
+			quad_cooler: 80, 
+			plasma_transistors: 60, 
+			nanowires: 40, 
+			chip: 20
+		},
+		purple: {
+			nicolascagium: 90, 
+			keanureevesium: 80, 
+			jimcarrium: 60, 
+			garyoldmanium: 40, 
+			amethyst_plasmoid: 20
+		}, 
+		orange: {
+			topaz_plasmoid: 90, 
+			ancient_technology: 80, 
+			ancient_scheme: 60, 
+			ancient_knowledge: 40, 
+			ancient_artefact: 20 
+		},
+		red: {
+			ruby_plasmoid: 50
+		}
 	},
 	arid: {
-		white: { weapon_parts: 100, silver_plasmoid: 80, ship_details: 60, meteor_fragments: 40, crystal_fragments: 20 },
-		green: { secret_technology: 100, rotary_amplifier: 80, reptile_technology: 60, emerald_plasmoid: 40, batteries: 20 },
-		blue: { sapphire_plasmoid: 100, quad_cooler: 80, plasma_transistors: 60, nanowires: 40, chip: 20 },
-		purple: { nicolascagium: 100, keanureevesium: 80, jimcarrium: 60, garyoldmanium: 40, amethyst_plasmoid: 20 }, 
-		orange: { topaz_plasmoid: 100, ancient_technology: 80, ancient_scheme: 60, ancient_knowledge: 40, ancient_artefact: 20 },
-		red: { ruby_plasmoid: 100 }
+		white: {
+			weapon_parts: 90, 
+			silver_plasmoid: 80, 
+			ship_details: 60, 
+			meteor_fragments: 40, 
+			crystal_fragments: 20
+		},
+		green: {
+			secret_technology: 90, 
+			rotary_amplifier: 80, 
+			reptile_technology: 60, 
+			emerald_plasmoid: 40, 
+			batteries: 20
+		},
+		blue: {
+			sapphire_plasmoid: 90, 
+			quad_cooler: 80, 
+			plasma_transistors: 60, 
+			nanowires: 40, 
+			chip: 20
+		},
+		purple: {
+			nicolascagium: 90, 
+			keanureevesium: 80, 
+			jimcarrium: 60, 
+			garyoldmanium: 40, 
+			amethyst_plasmoid: 20
+		}, 
+		orange: {
+			topaz_plasmoid: 90, 
+			ancient_technology: 80, 
+			ancient_scheme: 60, 
+			ancient_knowledge: 40, 
+			ancient_artefact: 20 
+		},
+		red: {
+			ruby_plasmoid: 50
+		}
 	},
 	tundra: {
-		white: { weapon_parts: 100, silver_plasmoid: 80, ship_details: 60, meteor_fragments: 40, crystal_fragments: 20 },
-		green: { secret_technology: 100, rotary_amplifier: 80, reptile_technology: 60, emerald_plasmoid: 40, batteries: 20 },
-		blue: { sapphire_plasmoid: 100, quad_cooler: 80, plasma_transistors: 60, nanowires: 40, chip: 20 },
-		purple: { nicolascagium: 100, keanureevesium: 80, jimcarrium: 60, garyoldmanium: 40, amethyst_plasmoid: 20 }, 
-		orange: { topaz_plasmoid: 100, ancient_technology: 80, ancient_scheme: 60, ancient_knowledge: 40, ancient_artefact: 20 },
-		red: { ruby_plasmoid: 100 }
+		white: {
+			weapon_parts: 90, 
+			silver_plasmoid: 80, 
+			ship_details: 60, 
+			meteor_fragments: 40, 
+			crystal_fragments: 20
+		},
+		green: {
+			secret_technology: 90, 
+			rotary_amplifier: 80, 
+			reptile_technology: 60, 
+			emerald_plasmoid: 40, 
+			batteries: 20
+		},
+		blue: {
+			sapphire_plasmoid: 90, 
+			quad_cooler: 80, 
+			plasma_transistors: 60, 
+			nanowires: 40, 
+			chip: 20
+		},
+		purple: {
+			nicolascagium: 90, 
+			keanureevesium: 80, 
+			jimcarrium: 60, 
+			garyoldmanium: 40, 
+			amethyst_plasmoid: 20
+		}, 
+		orange: {
+			topaz_plasmoid: 90, 
+			ancient_technology: 80, 
+			ancient_scheme: 60, 
+			ancient_knowledge: 40, 
+			ancient_artefact: 20 
+		},
+		red: {
+			ruby_plasmoid: 50
+		}
 	},
 	desert: {
-		white: { weapon_parts: 100, silver_plasmoid: 80, ship_details: 60, meteor_fragments: 40, crystal_fragments: 20 },
-		green: { secret_technology: 100, rotary_amplifier: 80, reptile_technology: 60, emerald_plasmoid: 40, batteries: 20 },
-		blue: { sapphire_plasmoid: 100, quad_cooler: 80, plasma_transistors: 60, nanowires: 40, chip: 20 },
-		purple: { nicolascagium: 100, keanureevesium: 80, jimcarrium: 60, garyoldmanium: 40, amethyst_plasmoid: 20 }, 
-		orange: { topaz_plasmoid: 100, ancient_technology: 80, ancient_scheme: 60, ancient_knowledge: 40, ancient_artefact: 20 },
-		red: { ruby_plasmoid: 100 }
+		white: {
+			weapon_parts: 90, 
+			silver_plasmoid: 80, 
+			ship_details: 60, 
+			meteor_fragments: 40, 
+			crystal_fragments: 20
+		},
+		green: {
+			secret_technology: 90, 
+			rotary_amplifier: 80, 
+			reptile_technology: 60, 
+			emerald_plasmoid: 40, 
+			batteries: 20
+		},
+		blue: {
+			sapphire_plasmoid: 90, 
+			quad_cooler: 80, 
+			plasma_transistors: 60, 
+			nanowires: 40, 
+			chip: 20
+		},
+		purple: {
+			nicolascagium: 90, 
+			keanureevesium: 80, 
+			jimcarrium: 60, 
+			garyoldmanium: 40, 
+			amethyst_plasmoid: 20
+		}, 
+		orange: {
+			topaz_plasmoid: 90, 
+			ancient_technology: 80, 
+			ancient_scheme: 60, 
+			ancient_knowledge: 40, 
+			ancient_artefact: 20 
+		},
+		red: {
+			ruby_plasmoid: 50
+		}
 	},
 	arctic: {
-		white: { weapon_parts: 100, silver_plasmoid: 80, ship_details: 60, meteor_fragments: 40, crystal_fragments: 20 },
-		green: { secret_technology: 100, rotary_amplifier: 80, reptile_technology: 60, emerald_plasmoid: 40, batteries: 20 },
-		blue: { sapphire_plasmoid: 100, quad_cooler: 80, plasma_transistors: 60, nanowires: 40, chip: 20 },
-		purple: { nicolascagium: 100, keanureevesium: 80, jimcarrium: 60, garyoldmanium: 40, amethyst_plasmoid: 20 }, 
-		orange: { topaz_plasmoid: 100, ancient_technology: 80, ancient_scheme: 60, ancient_knowledge: 40, ancient_artefact: 20 },
-		red: { ruby_plasmoid: 100 }
+		white: {
+			weapon_parts: 90, 
+			silver_plasmoid: 80, 
+			ship_details: 60, 
+			meteor_fragments: 40, 
+			crystal_fragments: 20
+		},
+		green: {
+			secret_technology: 90, 
+			rotary_amplifier: 80, 
+			reptile_technology: 60, 
+			emerald_plasmoid: 40, 
+			batteries: 20
+		},
+		blue: {
+			sapphire_plasmoid: 90, 
+			quad_cooler: 80, 
+			plasma_transistors: 60, 
+			nanowires: 40, 
+			chip: 20
+		},
+		purple: {
+			nicolascagium: 90, 
+			keanureevesium: 80, 
+			jimcarrium: 60, 
+			garyoldmanium: 40, 
+			amethyst_plasmoid: 20
+		}, 
+		orange: {
+			topaz_plasmoid: 90, 
+			ancient_technology: 80, 
+			ancient_scheme: 60, 
+			ancient_knowledge: 40, 
+			ancient_artefact: 20 
+		},
+		red: {
+			ruby_plasmoid: 50
+		}
 	},
 	lava: {
-		white: { weapon_parts: 100, silver_plasmoid: 80, ship_details: 60, meteor_fragments: 40, crystal_fragments: 20 },
-		green: { secret_technology: 100, rotary_amplifier: 80, reptile_technology: 60, emerald_plasmoid: 40, batteries: 20 },
-		blue: { sapphire_plasmoid: 100, quad_cooler: 80, plasma_transistors: 60, nanowires: 40, chip: 20 },
-		purple: { nicolascagium: 100, keanureevesium: 80, jimcarrium: 60, garyoldmanium: 40, amethyst_plasmoid: 20 }, 
-		orange: { topaz_plasmoid: 100, ancient_technology: 80, ancient_scheme: 60, ancient_knowledge: 40, ancient_artefact: 20 },
-		red: { ruby_plasmoid: 100 }
+		white: {
+			weapon_parts: 90, 
+			silver_plasmoid: 80, 
+			ship_details: 60, 
+			meteor_fragments: 40, 
+			crystal_fragments: 20
+		},
+		green: {
+			secret_technology: 90, 
+			rotary_amplifier: 80, 
+			reptile_technology: 60, 
+			emerald_plasmoid: 40, 
+			batteries: 20
+		},
+		blue: {
+			sapphire_plasmoid: 90, 
+			quad_cooler: 80, 
+			plasma_transistors: 60, 
+			nanowires: 40, 
+			chip: 20
+		},
+		purple: {
+			nicolascagium: 90, 
+			keanureevesium: 80, 
+			jimcarrium: 60, 
+			garyoldmanium: 40, 
+			amethyst_plasmoid: 20
+		}, 
+		orange: {
+			topaz_plasmoid: 90, 
+			ancient_technology: 80, 
+			ancient_scheme: 60, 
+			ancient_knowledge: 40, 
+			ancient_artefact: 20 
+		},
+		red: {
+			ruby_plasmoid: 50
+		}
 	},
 	barren: {
-		white: { weapon_parts: 100, silver_plasmoid: 80, ship_details: 60, meteor_fragments: 40, crystal_fragments: 20 },
-		green: { secret_technology: 100, rotary_amplifier: 80, reptile_technology: 60, emerald_plasmoid: 40, batteries: 20 },
-		blue: { sapphire_plasmoid: 100, quad_cooler: 80, plasma_transistors: 60, nanowires: 40, chip: 20 },
-		purple: { nicolascagium: 100, keanureevesium: 80, jimcarrium: 60, garyoldmanium: 40, amethyst_plasmoid: 20 }, 
-		orange: { topaz_plasmoid: 100, ancient_technology: 80, ancient_scheme: 60, ancient_knowledge: 40, ancient_artefact: 20 },
-		red: { ruby_plasmoid: 100 }
+		white: {
+			weapon_parts: 90, 
+			silver_plasmoid: 80, 
+			ship_details: 60, 
+			meteor_fragments: 40, 
+			crystal_fragments: 20
+		},
+		green: {
+			secret_technology: 90, 
+			rotary_amplifier: 80, 
+			reptile_technology: 60, 
+			emerald_plasmoid: 40, 
+			batteries: 20
+		},
+		blue: {
+			sapphire_plasmoid: 90, 
+			quad_cooler: 80, 
+			plasma_transistors: 60, 
+			nanowires: 40, 
+			chip: 20
+		},
+		purple: {
+			nicolascagium: 90, 
+			keanureevesium: 80, 
+			jimcarrium: 60, 
+			garyoldmanium: 40, 
+			amethyst_plasmoid: 20
+		}, 
+		orange: {
+			topaz_plasmoid: 90, 
+			ancient_technology: 80, 
+			ancient_scheme: 60, 
+			ancient_knowledge: 40, 
+			ancient_artefact: 20 
+		},
+		red: {
+			ruby_plasmoid: 50
+		}
 	},
 	asteroids: {
-		white: { weapon_parts: 100, silver_plasmoid: 80, ship_details: 60, meteor_fragments: 40, crystal_fragments: 20 },
-		green: { secret_technology: 100, rotary_amplifier: 80, reptile_technology: 60, emerald_plasmoid: 40, batteries: 20 },
-		blue: { sapphire_plasmoid: 100, quad_cooler: 80, plasma_transistors: 60, nanowires: 40, chip: 20 },
-		purple: { nicolascagium: 100, keanureevesium: 80, jimcarrium: 60, garyoldmanium: 40, amethyst_plasmoid: 20 }, 
-		orange: { topaz_plasmoid: 100, ancient_technology: 80, ancient_scheme: 60, ancient_knowledge: 40, ancient_artefact: 20 },
-		red: { ruby_plasmoid: 100 }
+		white: {
+			weapon_parts: 90, 
+			silver_plasmoid: 80, 
+			ship_details: 60, 
+			meteor_fragments: 40, 
+			crystal_fragments: 20
+		},
+		green: {
+			secret_technology: 90, 
+			rotary_amplifier: 80, 
+			reptile_technology: 60, 
+			emerald_plasmoid: 40, 
+			batteries: 20
+		},
+		blue: {
+			sapphire_plasmoid: 90, 
+			quad_cooler: 80, 
+			plasma_transistors: 60, 
+			nanowires: 40, 
+			chip: 20
+		},
+		purple: {
+			nicolascagium: 90, 
+			keanureevesium: 80, 
+			jimcarrium: 60, 
+			garyoldmanium: 40, 
+			amethyst_plasmoid: 20
+		}, 
+		orange: {
+			topaz_plasmoid: 90, 
+			ancient_technology: 80, 
+			ancient_scheme: 60, 
+			ancient_knowledge: 40, 
+			ancient_artefact: 20 
+		},
+		red: {
+			ruby_plasmoid: 50
+		}
 	},
 	methane: {
-		white: { weapon_parts: 100, silver_plasmoid: 80, ship_details: 60, meteor_fragments: 40, crystal_fragments: 20 },
-		green: { secret_technology: 100, rotary_amplifier: 80, reptile_technology: 60, emerald_plasmoid: 40, batteries: 20 },
-		blue: { sapphire_plasmoid: 100, quad_cooler: 80, plasma_transistors: 60, nanowires: 40, chip: 20 },
-		purple: { nicolascagium: 100, keanureevesium: 80, jimcarrium: 60, garyoldmanium: 40, amethyst_plasmoid: 20 }, 
-		orange: { topaz_plasmoid: 100, ancient_technology: 80, ancient_scheme: 60, ancient_knowledge: 40, ancient_artefact: 20 },
-		red: { ruby_plasmoid: 100 }
+		white: {
+			weapon_parts: 90, 
+			silver_plasmoid: 80, 
+			ship_details: 60, 
+			meteor_fragments: 40, 
+			crystal_fragments: 20
+		},
+		green: {
+			secret_technology: 90, 
+			rotary_amplifier: 80, 
+			reptile_technology: 60, 
+			emerald_plasmoid: 40, 
+			batteries: 20
+		},
+		blue: {
+			sapphire_plasmoid: 90, 
+			quad_cooler: 80, 
+			plasma_transistors: 60, 
+			nanowires: 40, 
+			chip: 20
+		},
+		purple: {
+			nicolascagium: 90, 
+			keanureevesium: 80, 
+			jimcarrium: 60, 
+			garyoldmanium: 40, 
+			amethyst_plasmoid: 20
+		}, 
+		orange: {
+			topaz_plasmoid: 90, 
+			ancient_technology: 80, 
+			ancient_scheme: 60, 
+			ancient_knowledge: 40, 
+			ancient_artefact: 20 
+		},
+		red: {
+			ruby_plasmoid: 50
+		}
 	},
 	hydrogen: {
-		white: { weapon_parts: 100, silver_plasmoid: 80, ship_details: 60, meteor_fragments: 40, crystal_fragments: 20 },
-		green: { secret_technology: 100, rotary_amplifier: 80, reptile_technology: 60, emerald_plasmoid: 40, batteries: 20 },
-		blue: { sapphire_plasmoid: 100, quad_cooler: 80, plasma_transistors: 60, nanowires: 40, chip: 20 },
-		purple: { nicolascagium: 100, keanureevesium: 80, jimcarrium: 60, garyoldmanium: 40, amethyst_plasmoid: 20 }, 
-		orange: { topaz_plasmoid: 100, ancient_technology: 80, ancient_scheme: 60, ancient_knowledge: 40, ancient_artefact: 20 },
-		red: { ruby_plasmoid: 100 }
+		white: {
+			weapon_parts: 90, 
+			silver_plasmoid: 80, 
+			ship_details: 60, 
+			meteor_fragments: 40, 
+			crystal_fragments: 20
+		},
+		green: {
+			secret_technology: 90, 
+			rotary_amplifier: 80, 
+			reptile_technology: 60, 
+			emerald_plasmoid: 40, 
+			batteries: 20
+		},
+		blue: {
+			sapphire_plasmoid: 90, 
+			quad_cooler: 80, 
+			plasma_transistors: 60, 
+			nanowires: 40, 
+			chip: 20
+		},
+		purple: {
+			nicolascagium: 90, 
+			keanureevesium: 80, 
+			jimcarrium: 60, 
+			garyoldmanium: 40, 
+			amethyst_plasmoid: 20
+		}, 
+		orange: {
+			topaz_plasmoid: 90, 
+			ancient_technology: 80, 
+			ancient_scheme: 60, 
+			ancient_knowledge: 40, 
+			ancient_artefact: 20 
+		},
+		red: {
+			ruby_plasmoid: 50
+		}
 	},
 	helium: {
-		white: { weapon_parts: 100, silver_plasmoid: 80, ship_details: 60, meteor_fragments: 40, crystal_fragments: 20 },
-		green: { secret_technology: 100, rotary_amplifier: 80, reptile_technology: 60, emerald_plasmoid: 40, batteries: 20 },
-		blue: { sapphire_plasmoid: 100, quad_cooler: 80, plasma_transistors: 60, nanowires: 40, chip: 20 },
-		purple: { nicolascagium: 100, keanureevesium: 80, jimcarrium: 60, garyoldmanium: 40, amethyst_plasmoid: 20 }, 
-		orange: { topaz_plasmoid: 100, ancient_technology: 80, ancient_scheme: 60, ancient_knowledge: 40, ancient_artefact: 20 },
-		red: { ruby_plasmoid: 100 }
+		white: {
+			weapon_parts: 90, 
+			silver_plasmoid: 80, 
+			ship_details: 60, 
+			meteor_fragments: 40, 
+			crystal_fragments: 20
+		},
+		green: {
+			secret_technology: 90, 
+			rotary_amplifier: 80, 
+			reptile_technology: 60, 
+			emerald_plasmoid: 40, 
+			batteries: 20
+		},
+		blue: {
+			sapphire_plasmoid: 90, 
+			quad_cooler: 80, 
+			plasma_transistors: 60, 
+			nanowires: 40, 
+			chip: 20
+		},
+		purple: {
+			nicolascagium: 90, 
+			keanureevesium: 80, 
+			jimcarrium: 60, 
+			garyoldmanium: 40, 
+			amethyst_plasmoid: 20
+		}, 
+		orange: {
+			topaz_plasmoid: 90, 
+			ancient_technology: 80, 
+			ancient_scheme: 60, 
+			ancient_knowledge: 40, 
+			ancient_artefact: 20 
+		},
+		red: {
+			ruby_plasmoid: 50
+		}
 	}
 };
 
@@ -123,30 +539,75 @@ Game.Cosmos.PLANET_TYPE_ARTEFACTS = {
 // power - multiplier for item drop chance
 Game.Cosmos.ARTEFACTS_GROUP_SPREAD = [
 	{
-		white: { chance: 100, power: 1 }
+		white: {
+			chance: 100, 
+			power: 1
+		}
 	}, {
-		green: { chance: 100, power: 1 }
+		green: {
+			chance: 100, 
+			power: 1
+		}
 	}, {
-		green: { chance: 75, power: 1 },
-		blue: { chance: 25, power: 0.5 }
+		green: {
+			chance: 75,
+			power: 1
+		},
+		blue: { 
+			chance: 25,
+			power: 0.5
+		}
 	}, {
-		blue: { chance: 100, power: 1 }
+		blue: {
+			chance: 100,
+			power: 1
+		}
 	}, {
-		blue: { chance: 75, power: 1 },
-		purple: { chance: 25, power: 0.5 }
+		blue: {
+			chance: 75,
+			power: 1
+		},
+		purple: {
+			chance: 25,
+			power: 0.5
+		}
 	}, {
-		purple: { chance: 100, power: 1 }
+		purple: {
+			chance: 100,
+			power: 1
+		}
 	}, {
-		purple: { chance: 75, power: 1 },
-		orange: { chance: 25, power: 0.5 }
+		purple: {
+			chance: 75,
+			power: 1
+		},
+		orange: {
+			chance: 25,
+			power: 0.5
+		}
 	}, {
-		orange: { chance: 100, power: 1 }
+		orange: {
+			chance: 100,
+			power: 1
+		}
 	}, {
-		orange: { chance: 75, power: 1 },
-		red: {  chance: 25, power: 0.5 }
+		orange: {
+			chance: 75,
+			power: 1
+		},
+		red: {
+			chance: 25,
+			power: 0.5
+		}
 	}, {
-		orange: { chance: 50, power: 1 },
-		red: { chance: 50, power: 1 }
+		orange: {
+			chance: 50,
+			power: 1
+		},
+		red: {
+			chance: 50,
+			power: 1
+		}
 	}
 ];
 
