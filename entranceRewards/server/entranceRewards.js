813ee434c366bbfb6edd539f9709f99770f8f607
Vladimir Karasev <mrkitan@gmail.com>
2017-03-03 15:35:14 +0200

    CSWE-146: init entrance reward content

---
+++ entranceRewards/server/entranceRewards.js
@@ -0,0 +1,24 @@
+initEntranceRewardsContent = function() {
+
+new game.EntranceReward({
+	seqNum: 0,
+	reward: {
+		metals: 600
+	}
+});
+
+new game.EntranceReward({
+	seqNum: 1,
+	reward: {
+		crystals: 200
+	}
+});
+
+new game.EntranceReward({
+	seqNum: 2,
+	reward: {
+		crystals: 300
+	}
+});
+
+};
