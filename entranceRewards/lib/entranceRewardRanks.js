754e3f8b929f044b71ec9053340ab20b17776c7c
Vladimir Karasev <mrkitan@gmail.com>
2017-03-09 18:01:45 +0200

    CSWE-146: add ranks

---
+++ entranceRewards/lib/entranceRewardRanks.js
@@ -0,0 +1,51 @@
+initEntranceRewardRanksContent = function() {
+
+new game.EntranceRewardRank({
+	id: 'white',
+	rewards: [
+		{ resources: { metals: 600 } },
+		{ units: { ground: { fathers: 42 } } }
+	]
+});
+
+new game.EntranceRewardRank({
+	id: 'green',
+	rewards: [
+		{ resources: { metals: 600 } },
+		{ units: { ground: { fathers: 42 } } }
+	]
+});
+
+new game.EntranceRewardRank({
+	id: 'blue',
+	rewards: [
+		{ resources: { metals: 600 } },
+		{ units: { ground: { fathers: 42 } } }
+	]
+});
+
+new game.EntranceRewardRank({
+	id: 'purple',
+	rewards: [
+		{ resources: { metals: 600 } },
+		{ units: { ground: { fathers: 42 } } }
+	]
+});
+
+new game.EntranceRewardRank({
+	id: 'orange',
+	rewards: [
+		{ resources: { metals: 600 } },
+		{ units: { ground: { fathers: 42 } } }
+	]
+});
+
+new game.EntranceRewardRank({
+	id: 'red',
+	rewards: [
+		{ resources: { metals: 600 } },
+		{ units: { ground: { fathers: 42 } } }
+	]
+});
+
+};
--- entranceRewards/lib/entranceRewards.js
+++ entranceRewards/lib/entranceRewards.js
@@ -1,58 +1,74 @@
 initEntranceRewardsContent = function() {
 
+initEntranceRewardRanksContent();
+
 new game.EntranceReward({
 	day: 0,
 	profit: {
-		resource: { metals: 600 }
+		rank: 'white'
 	}
 });
 
 new game.EntranceReward({
 	day: 1,
 	profit: {
-		resource: { crystals: 200 }
+		resources: { crystals: 200 }
 	}
 });
 
 new game.EntranceReward({
 	day: 2,
 	profit: {
-		resource: { crystals: 300 }
+		resources: { crystals: 300 }
 	}
 });
 
 new game.EntranceReward({
 	day: [3, 5],
 	profit: {
-		resource: { crystals: 350 }
+		resources: { crystals: 350 }
 	}
 });
 
 new game.EntranceReward({
 	day: [6, 10],
 	profit: {
-		resource: { humans: 40 }
+		resources: { humans: 40 }
 	}
 });
 
 new game.EntranceReward({
 	day: [11, 14],
 	profit: {
-		unit: { ground: { fathers: 42 } }
+		units: { ground: { fathers: 42 } }
 	}
 });
 
 new game.EntranceReward({
 	day: [15, 25],
 	profit: {
-		unit: { ground: { horizontalbarman: 42 } }
+		units: { ground: { horizontalbarman: 42 } }
+	}
+});
+
+new game.EntranceReward({
+	day: 26,
+	profit: {
+		rank: 'white'
+	}
+});
+
+new game.EntranceReward({
+	day: 27,
+	profit: {
+		rank: 'green'
 	}
 });
 
 new game.EntranceReward({
 	day: 'default',
 	profit: {
-		unit: { ground: { fathers: 42 } }
+		rank: 'purple'
 	}
 });
 
