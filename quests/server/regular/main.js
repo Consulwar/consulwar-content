be7a96a2022bd32fda0573b9fe962ec189b54d49
Irina Reinova <reinova@hotmail.com>
2017-03-19 23:10:06 +0300

    CSWE-21: цепочка квестов для Колизея

--- quests/server/regular/main.js
+++ quests/server/regular/main.js
@@ -14,6 +14,7 @@ initQuestRegularContent = function() {
 	initQuestRegularContentSpaceportBuilding();
 	initQuestRegularContentTradingportBuilding();
 	initQuestRegularContentPulsecatcherBuilding();
+	initQuestRegularContentColosseumBuilding();
 	initQuestRegularContentCrystalBuilding();
 	initQuestRegularContentBlackmarketBuilding();
 	initQuestRegularContentPowerstationBuilding();
