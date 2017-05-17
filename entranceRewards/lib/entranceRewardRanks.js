563866e5a06680c53191743d78db2c0f94b53b82
Alexandr Zelenin <zav.work@gmail.com>
2017-05-17 11:57:31 +0200

    CSWE-249: rewards for 61-120 days

--- entranceRewards/lib/entranceRewardRanks.js
+++ entranceRewards/lib/entranceRewardRanks.js
@@ -61,27 +61,27 @@ new game.EntranceRewardRank({
 	}]
 });
 
-// Not ready
 new game.EntranceRewardRank({
 	engName: 'purple',
 	rewards: [{
 		chance: 1,
-		profit: { units: { ground: { mothertank: 1 } } }
+		profit: { units: { fleet: { dreadnought: 50 } } }
 	}, {
-		chance: 19,
-		profit: { units: { fleet: { wasp: 50 } } }
+		chance: 9,
+		profit: { units: { ground: { hbhr: 2 } } }
 	}, {
-		chance: 20,
-		profit: { units: { ground: { agmogedcar: 10 } } }
-	}, {
-		chance: 30,
-		profit: { resources: { crystals: 10000 } }
+		chance: 25,
+		profit: { units: { defense: { bomb: 1500 } } }
 	}, {
 		chance: 30,
-		profit: { resources: { metals: 25000 } }
+		profit: { resources: { crystals: 120000 } }
+	}, {
+		chance: 35,
+		profit: { resources: { metals: 250000 } }
 	}]
 });
 
+// Not ready
 new game.EntranceRewardRank({
 	engName: 'orange',
 	rewards: [{
